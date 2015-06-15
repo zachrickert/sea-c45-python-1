cfab5dbe633164f6a7c2979ebf9071bfa8123e0b
Paul Pham <paul@codefellows.com>
2015-06-15 16:44:39 -0700

    Add unit test for break_me.py

---
+++ hw/hw1/test_break_me.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+code that tests the break_me.py functions
+"""
+
+# import pytest  # used for the exception testing
+
+
+import break_me
+
+
+def test_name_error():
+    try:
+        break_me.exhibit_name_error()
+        assert(False)
+    except NameError:
+        assert(True)
+
+
+def test_attribute_error():
+    try:
+        break_me.exhibit_attribute_error()
+        assert(False)
+    except AttributeError:
+        assert(True)
+
+
+def test_type_error():
+    try:
+        break_me.exhibit_type_error()
+        assert(False)
+    except TypeError:
+        assert(True)
