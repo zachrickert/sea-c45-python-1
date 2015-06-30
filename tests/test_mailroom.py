9b97457aa4212b0bc4cc84f48ead805a91b12282
henrykh <henryhowes@gmail.com>
2015-06-29 19:58:20 -0700

    adds final version of mailroom tests

--- tests/test_mailroom.py
+++ tests/test_mailroom.py
@@ -1,16 +1,16 @@
-import pytest
 from subprocess import Popen, PIPE, STDOUT, TimeoutExpired
+
 
 def process(input):
     p = Popen(['python', './mailroom.py'],
-          stdout=PIPE,
-          stdin=PIPE,
-          stderr=STDOUT)
+              stdout=PIPE,
+              stdin=PIPE,
+              stderr=STDOUT)
+
     try:
         mailroom_stdout = p.communicate(
             input=input, timeout=3)[0]
         output = mailroom_stdout.decode().lower()
-        output = output.split(":")
 
         return output
 
@@ -20,35 +20,36 @@ def process(input):
 
 def test_quit():
     output = process(b'quit')
-    assert("send a thank you" in output[0])
-    assert("create a report" in output[0])
+    assert("send a (t)hank you" in output)
+    assert("create a (r)eport" in output)
+
 
 def test_thank_you_quit():
     output = process(b'T\nlist\nquit')
-
     # prompts for a name
-    assert("enter a name" in output[1])
-    # prints a list
-    assert("[" and "]" in output[2])
+    assert("enter a name" in output)
+
 
 def test_name_quit():
     output = process(b'T\nbill gates\nquit')
-    assert("donation amount" in output[3])
+    assert("donation amount" in output)
+
 
 def test_name_donate():
     output = process(b'T\nbill gates\n200\nquit')
 
     # name and amount in thank you
-    assert("bill gates" in output[3])
-    assert("200" in output[3])
+    assert("bill gates" in output)
+    assert("200" in output)
 
     # successfully returns to main menu
-    assert("send a thank you" in output[3])
-    assert("create a report" in output[3])
+    assert("send a (t)hank you" in output)
+    assert("create a (r)eport" in output)
+
 
 def test_name_report():
     output = process(b'T\nbill gates\n200\nR\nquit')
 
     # new donor in report
-    assert("bill gates" in output[4])
-    assert("200" in output[4])
+    assert("bill gates" in output)
+    assert("200" in output)
