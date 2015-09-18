33a3c70e473ed4587bf8db5c63d2f7f07e7b3e8e
Zach Rickert <zachrickert@gmail.com>
2015-09-18 01:36:21 -0700

    Add program for the lucas series.

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -4,6 +4,7 @@ def fibonacci(n):
 
     Example:    fibonacci(0) == 0
                 fibonacci(1) == 1
+                fibonacci(n) == fibonacci(n-1) + fibonacci(n-2)
 
     """
 
@@ -13,5 +14,25 @@ def fibonacci(n):
         x = 1
     else:
         x = fibonacci(n - 1) + fibonacci(n - 2)
+
+    return x
+
+
+def lucas(n):
+    """
+    Return the nth number from the lucas sequence
+
+    Example:    lucas(0) == 2
+                lucas(1) == 1
+                lucas(n) == lucas(n-1) + lucas(n-2)
+
+    """
+
+    if (n == 0):
+        x = 2
+    elif (n == 1):
+        x = 1
+    else:
+        x = lucas(n - 1) + lucas(n - 2)
 
     return x
