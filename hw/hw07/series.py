4874db79088926fbffab6c6cdc0ccd3f17473f76
Zach Rickert <zachrickert@gmail.com>
2015-09-18 01:31:21 -0700

    Add program for the fibonacci series.

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -7,4 +7,11 @@ def fibonacci(n):
 
     """
 
-    return 1
+    if (n == 0):
+        x = 0
+    elif (n == 1):
+        x = 1
+    else:
+        x = fibonacci(n - 1) + fibonacci(n - 2)
+
+    return x
