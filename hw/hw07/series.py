9a70c7bcabc85ef99dba35794a5f73df152a1e97
Zach Rickert <zachrickert@gmail.com>
2015-09-18 01:46:23 -0700

    Add program for the sum_series. Pass py.test

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -36,3 +36,24 @@ def lucas(n):
         x = lucas(n - 1) + lucas(n - 2)
 
     return x
+
+
+def sum_series(n, n0, n1):
+    """
+    Return the nth number from a sum sequence
+
+    Example:    sum_series(0, n0, n1) == n0
+                sum_series(1, n0, n1) == n1
+                sum_series(n, n0, n1) ==
+                    sum_series(n-1, n0, n1) + sum_series(n-2, n0, n1)
+
+    """
+
+    if (n == 0):
+        x = n0
+    elif (n == 1):
+        x = n1
+    else:
+        x = sum_series(n - 1, n0, n1) + sum_series(n - 2, n0, n1)
+
+    return x
