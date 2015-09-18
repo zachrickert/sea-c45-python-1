3cc8f44fcdb59a863567d121e587da82232e4156
Zach Rickert <zachrickert@gmail.com>
2015-09-18 03:56:11 -0700

    Complete code and answer for problem 5.

--- hw/hw08/dna_analysis.py
+++ hw/hw08/dna_analysis.py
@@ -49,9 +49,14 @@ for line in inputfile:
 
 # Total nucleotides seen so far.
 total_count = 0
-# Number of G and C nucleotides seen so far.
+# Number of nucleotides seen so far.
 gc_count = 0
 at_count = 0
+
+g_count = 0
+c_count = 0
+a_count = 0
+t_count = 0
 
 
 # for each base pair in the string,
@@ -60,12 +65,20 @@ for bp in seq:
     total_count = total_count + 1
 
     # next, if the bp is a G or a C,
-    if bp == 'C' or bp == 'G':
-        # increment the count of gc
+    if bp == 'C':
+        c_count = c_count + 1
         gc_count = gc_count + 1
 
-    if bp == 'A' or bp == 'T':
-        # increment the count of at
+    if bp == 'G':
+        g_count = g_count + 1
+        gc_count = gc_count + 1
+
+    if bp == 'A':
+        a_count = a_count + 1
+        at_count = at_count + 1
+
+    if bp == 'T':
+        t_count = t_count + 1
         at_count = at_count + 1
 
 
@@ -76,3 +89,8 @@ at_content = float(at_count) / total_count
 # Print the answer
 print('GC-content:', gc_content)
 print('AT-content:', at_content)
+print()
+print('G-count:', g_count)
+print('C-count:', c_count)
+print('A-count:', a_count)
+print('T-count:', t_count)
