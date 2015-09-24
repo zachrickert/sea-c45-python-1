8c33a83b4b4ecd12cb4bce0d1acb2bc98c94235f
Zach Rickert <zachrickert@gmail.com>
2015-09-23 18:52:11 -0700

    Start Psuedo Code.

---
+++ hw/hw10/mailroom.py
@@ -0,0 +1,50 @@
+def initial_prompt():
+    """ Request data from the user.
+
+    Inputs are either:
+    1) Send Thank you
+    2) Create Report
+    3) Quit
+
+    calls validate_initial_prompt to verify results
+    if not verified will ask user to Inputs
+
+    returns value
+
+    """
+
+
+def validate_initial_prompt(text):
+    """ Validates user input at the initial prompt.
+
+    returns True if data is in correct form
+    returns False if it is in an unknown input."""
+
+
+def enter_name():
+    """ Will prompt user to enter donor's name.
+
+    Inputs would be:
+    1) Full Name
+    2) List
+    2) Return to main menu.
+
+    returns full name or command to llist all donors.
+    """
+
+
+def check_prior_donations(donor_name):
+    """Compares name to names of people who have given in the past.
+
+    Returns True if the name is in the database.
+    Returns False if the name is not in the database.
+
+    """
+
+def create_donor(donor_name):
+    """ Adds a new donor to the list of donors.
+    Sets up all initial fields."""
+
+def list_donors():
+    """ Lists all
+
