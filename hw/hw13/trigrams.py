954b00ac869ae44cbd6b454a7f5618bc44482d9a
Zach Rickert <zachrickert@gmail.com>
2015-09-27 17:58:48 -0700

    Add code to create a sentence.

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -1,14 +1,43 @@
-file = open("sherlock_small.txt", r)
+import random
+file = open("test.txt", 'r')
 
 lines = file.readlines()
 words = []
 trigrams = {}
+start_phrase = "I wish"
 
 for line in lines:
-    words += line.split(' ')
+    words += line.split()
 
-for i, word in enumerate(words):
-    trigrams[(word, words[i+1])] = words[i+2]
+for i in (range(len(words) - 2)):
+    phrase = words[i] + " " + words[i + 1]
+    if (phrase in trigrams):
+        trigrams[phrase].append(words[i + 2])
+    else:
+        trigrams[phrase] = [words[i + 2]]
 
-print(str(words))
-print(str(trigrams))
+done = False
+
+word_2 = start_phrase.split()[0]
+word_2 = "I"
+word_1 = start_phrase.split()[1]
+word_1 = "wish"
+sentance = start_phrase
+j = 0
+
+while not done:
+    j = j + 1
+    phrase = word_2 + " " + word_1
+    if (phrase in trigrams):
+        numb_of_words = len(trigrams[phrase]) - 1
+        i = random.randint(0, numb_of_words)
+        list_words = trigrams[phrase]
+        new_word = list_words[i]
+        sentance = sentance + " " + new_word
+        word_2 = word_1
+        word_1 = new_word
+
+    else:
+        done = True
+
+print(sentance)
