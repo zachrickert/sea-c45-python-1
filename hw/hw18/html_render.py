d9672d01a2a9c537661edc154a5095c5f9d27476
Zach Rickert <zachrickert@gmail.com>
2015-10-03 19:03:38 -0700

    Change format to match exactly.

--- hw/hw18/html_render.py
+++ hw/hw18/html_render.py
@@ -13,30 +13,21 @@ class Element(object):
     def __init__(self, tag=""):
         self.tag = tag
         self.contents = []
+        self.pre = ''
+        self.post = ''
 
     def append(self, child):
-        # child = child.split('\n')
-        # for items in child:
-        #     self.contents.append(items)
         self.contents.append(child)
-
-    def print_to_terminal(self, ind_level):
-        tab = '    ' * ind_level
-        opening = "<{tag}>".format(tag=self.tag)
-        closing = "</{tag}>".format(tag=self.tag)
-        print(opening)
-        for child in self.contents:
-            child = tab + child
-            print (child)
-        print(closing)
 
     def render(self, f, ind=''):
         if(self.tag == 'html'):
-            f.write('<!DOCTYPE html>\n')
+            f.write(self.pre)
+        if(self.tag == ''):
+            f.write('\n')
 
         tab = '    ' + ind
         opening = ind + "<{tag}>\n".format(tag=self.tag)
-        closing = ind + "</{tag}>\n".format(tag=self.tag)
+        closing = ind + "</{tag}>{post}".format(tag=self.tag, post=self.post)
         f.write(opening)
         for child in self.contents:
             if (type(child) == str):
@@ -50,16 +41,19 @@ class Element(object):
 class Html(Element):
     def __init__(self):
         Element.__init__(self, "html")
+        self.pre = '<!DOCTYPE html>\n'
 
 
 class Head(Element):
     def __init__(self):
         Element.__init__(self, "head")
+        self.post = '\n'
 
 
 class Body(Element):
     def __init__(self):
         Element.__init__(self, "body")
+        self.post = '\n'
 
 
 class P(Element):
@@ -67,6 +61,7 @@ class P(Element):
         Element.__init__(self, "p")
         self.line = line
         self.append(line)
+        self.post = '\n'
 
 
 class Title(Element):
@@ -74,8 +69,10 @@ class Title(Element):
         Element.__init__(self, "title")
         self.line = line
         self.append(line)
+        self.post = '\n'
 
     def render(self, f, ind=''):
-        tline = "{t}<{tag}>{line}</{tag}>\n"
-        tline = tline.format(t=ind, tag=self.tag, line=self.line)
+        tline = "{t}<{tag}>{line}</{tag}>{end}"
+        tline = tline.format(t=ind, tag=self.tag, line=self.line,
+                             end=self.post)
         f.write(tline)
